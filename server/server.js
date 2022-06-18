2da2ae4d134dfdaf393e1187392d1104e7924b67
Glen <glen.kasoff@gmail.com>
2022-06-18 13:32:33 -0400

    stuck on backend

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const path = require('path')
+
+const app = express();
+
+app.get('/', (req, res) => {
+   return res.json({user: 'geek'})
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
