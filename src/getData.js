f7ce3ab67bb6ca7118d090f6f5172551bc0e0f1d
Glen <glen.kasoff@gmail.com>
2022-06-18 16:42:40 -0400

    backend good

--- src/getData.js
+++ src/getData.js
@@ -6,19 +6,18 @@ function getData() {
             Accept: 'application/json'
         }
     }
-    fetch('http://localhost:5000/')
-    .then(response => {
-        console.log('response', response.body)
-        response.json()
-    })
-    .then(data => console.log(data))
-
-    // axios.get('https://swapi.dev/api/', config)
-    // // .then(res => res.json())
-    // .then(res => {
-    //     console.log('thiz ', res)
-    //     document.getElementById('data').innerHTML = res.data.results[Math.floor(Math.random() * 10)].name
+    // fetch('http://localhost:5000/')
+    // .then(response => {
+    //     console.log('response', response.body)
+    //     response.json()
     // })
+    // .then(data => console.log(data))
+
+
+    axios.get('http://localhost:5000/', config).then(res => {
+        console.log('rezzz', res.data)
+        document.getElementById('data').innerHTML = res.data.user
+    })
 }
 
 export default getData
